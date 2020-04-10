4a0e1ba0fbccec5cc0dd580bbc4c44e5ec7bd767
pratikkuyate96 <62277306+pratikkuyate96@users.noreply.github.com>
2020-04-11 01:35:23 +0530

    ADDED: Checked email id valid or not

--- UserRegistration.sh
+++ UserRegistration.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-#Function check first name
+#Function to check first name valid or not
 function checkFirstName() {
-	NAMING_PATTERN="^[A-Z][a-z]{3,15}$"
+	NAMING_PATTERN="^[A-Z][a-z]{3}$"
 	firstName=""
 	read -p "Enter First Name : " firstName
 	if [[ $firstName =~ $NAMING_PATTERN ]]
@@ -13,16 +13,37 @@ function checkFirstName() {
 	fi
 }
 
-#Function check last name
+#Function to check last name valid or not
 function checkLastName() {
-	NAMING_PATTERN="^[A-Z][a-z]{3,15}$"
+	NAMING_PATTERN="^[A-Z][a-z]{3}$"
 	lastName=""
 	read -p "Ënter Last Name : " lastName
 	if [[ $lastName =~ $NAMING_PATTERN ]]
 	then
 		echo "The last name is Valid"
 	else
+
+
+
+
+
+
 		echo "The last name is Invalid"
+	fi
+}
+
+
+
+#Function to check Email Id valid or not
+function checkEmailId {
+	EMAIL_ID_PATTERN="^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$"
+	emailId=""
+	read -p "Enter Email Id : " emailId
+	if [[ $emailId =~ $EMAIL_ID_PATTERN ]]
+	then
+		echo "The email id is valid "
+	else
+		echo "The email id is invalid"
 	fi
 }
 
@@ -30,3 +51,4 @@ function checkLastName() {
 echo "~~~~~Welcome to User Registration~~~~"
 checkFirstName
 checkLastName
+checkEmailId
